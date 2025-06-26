cccecb02d5c58f6463907aead7a764fd4510a7e7
Marcos Bião <dev.marcosdly@gmail.com>
2025-06-26 08:53:03 -0300

    feat: add bun types and define __APP_META__ global

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+/// <reference types="bun-types" />
+
+declare const __APP_META__: {
+  isDev: boolean;
+  isProd: boolean;
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-})
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+
+  return {
+    plugins: [preact()],
+    define: {
+      __APP_META__: JSON.stringify({
+        isDev,
+        isProd,
+      }),
+    },
+  };
+});
