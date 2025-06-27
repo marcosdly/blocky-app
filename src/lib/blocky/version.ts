c21cb8e189d2bddb00e675b4fb0977e155af8198
Marcos Bião <dev.marcosdly@gmail.com>
2025-06-27 12:29:24 -0300

    feat: fetch version info

---
+++ src/lib/blocky/version.ts
@@ -0,0 +1,38 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+import { BLOCKY_EXECUTABLE_PATH } from "./process.ts";
+
+const execAsync = promisify(exec);
+
+export interface VersionStruct {
+  major: number;
+  minor: number;
+  patch: number;
+}
+
+export async function versionAsString(): Promise<string> {
+  let stdout: string;
+  try {
+    stdout = (await execAsync(`${BLOCKY_EXECUTABLE_PATH} version`)).stdout;
+    for (const line of stdout.split("\n")) {
+      if (line.startsWith("Version")) {
+        return line.slice("Version: ".length).trim();
+      }
+    }
+    throw new Error("Version not found in output");
+  } catch (error) {
+    throw new Error(`Failed to get version: ${(error as Error).message}`);
+  }
+}
+
+export async function versionAsStruct(): Promise<VersionStruct> {
+  const versionString = await versionAsString();
+  const [major, minor, patch] = versionString
+    .replace(/^v+/, "") // Remove leading 'v' if present
+    .split(".")
+    .map(Number);
+  if (isNaN(major) || isNaN(minor) || isNaN(patch)) {
+    throw new Error("Invalid version format");
+  }
+  return { major, minor, patch } as VersionStruct;
+}
